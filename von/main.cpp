e5f1732b72e39ba75955d3e96484b6cf25b56281
sailll choi <sailll@sina.cn>
2016-02-27 15:56:37 +0800

    Initial Commit

---
+++ von/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  von
+//
+//  Created by 蔡仲谋 on 2/27/16.
+//  Copyright © 2016 蔡仲谋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
