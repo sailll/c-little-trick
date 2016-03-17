b33ab18345bf900299b5bafbc17244fc6d701d3b
sailll choi <sailll@sina.cn>
2016-03-17 19:36:01 +0800

    change

--- von/primejudge.cpp
+++ von/primejudge.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#define N 5
 using namespace std;
 class solution
 {
@@ -19,12 +21,35 @@ public:
         cout<<"this is a prime number"<<endl;
         return ;
     }
+    void change(char *b)
+    {
+        for(int i=0;i<N;i++)
+        {
+            if(*(b+N-i-1)<=90)
+            {
+                *(b+N-i-1)+=32;
+                cout<<*(b+N-i-1)<<endl;
+            }
+            else
+            {
+                *(b+N-i-1)-=32;
+                cout<<*(b+N-i-1)<<endl;
+            }
+        }
+    }
 };
 int main()
 {
     solution po;
     int n;
+    char *b=new char[N];
     cin>>n;
     po.primejudge(n);
+    for(int i=0;i<N;i++)
+    {
+        cin>>*(b+i);
+    }
+    po.change(b);
+    delete []b;
     return 0;
 }
