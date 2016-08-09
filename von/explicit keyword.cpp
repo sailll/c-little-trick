7bb5587ffbd86e325f87b769adca13c18270054e
sailll choi <sailll@sina.cn>
2016-08-09 21:54:50 +0800

    explicit keyword
    using an explicit keyword before a function will disable the inexplicit conversion mechanism of the parameters

---
+++ von/explicit keyword.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<utility>
+#include<queue>
+using namespace std;
+class B{
+public:
+    int x;
+    B(int i=23){
+        this->x=i;
+    }
+    //using explicit keyword will disable the imexplicit conversion mechanism
+};
+void output(B ob){
+    cout<<ob.x<<endl;
+}
+int main(){
+    char s='2';
+    output(s);
+    return 0;
+}
